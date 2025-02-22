c84a70aab6e99325998e304da068dd8ce35f7884
mobtone <tonenordstrom@hotmail.com>
2025-02-22 20:36:12 +0100

    konfigurerat apinyckel genom .env

--- weather-app/src/services/WeatherService.js
+++ weather-app/src/services/WeatherService.js
@@ -1,8 +1,14 @@
 
-const getWeatherData = () => {
+export const getWeatherData = async (location) => {
+    
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-    fetch()
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric";
+   
+    const weatherData = await fetch(url).then(response => response.json());
+
+    return weatherData(location);
+   //"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={apiKey}";
 
 }
 
-export default getWeatherData;
--- weather-app/vite.config.js
+++ weather-app/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
